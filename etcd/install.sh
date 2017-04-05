da50b37ed204b7423efba80e72d2d1ff5291063a
Jason Brooks <jbrooks@redhat.com>
2017-04-05 15:47:37 -0700

    add script to run etcdctl from container

--- etcd/install.sh
+++ etcd/install.sh
@@ -3,6 +3,11 @@
 # Create etcd conf dir
 mkdir -p /etc/etcd && touch /etc/etcd/etcd.conf
 
+# put a script to run etcdctl from container in /usr/local/bin
+
+echo "runc exec -- etcd /usr/bin/etcdctl @" > /usr/local/bin/etcdctl
+chmod +x /usr/local/bin/etcdctl
+
 # Install systemd unit file for running container
 sed -e "s/TEMPLATE/${NAME}/g" /etc/systemd/system/etcd_container_template.service > ${HOST}/etc/systemd/system/etcd_container_${NAME}.service
 
