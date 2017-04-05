5942bf84573c09f14f9988b47236441d6880a654
Jason Brooks <jbrooks@redhat.com>
2017-04-05 15:30:53 -0700

    create flanneld conf file in host and bind mount in container

--- flannel/flanneld-run.sh
+++ flannel/flanneld-run.sh
@@ -4,6 +4,9 @@
 echo "[Service]" > /etc/systemd/system/docker.service.d/$NAME.conf
 echo "EnvironmentFile=-/run/$NAME/docker" >> /etc/systemd/system/docker.service.d/$NAME.conf
 
+# Create flannel config file
+touch /etc/sysconfig/flanneld
+
 # Ensure this file doesn't already exist.
 rm -f run/flannel/subnet.env
 
