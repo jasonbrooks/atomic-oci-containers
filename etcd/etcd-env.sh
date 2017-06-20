3a7c20e2101d18e17befcd9d556da324e846d948
Jason Brooks <jbrooks@redhat.com>
2017-06-20 09:47:30 -0700

    test data dir from conf file

--- etcd/etcd-env.sh
+++ etcd/etcd-env.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-ipaddress=$(hostname -I | cut -f1 -d' ')
-if test x$NAME == x; then
-    NAME=$HOSTNAME
-fi
-export ETCD_NAME=${ETCD_NAME:-$HOSTNAME}
-export ETCD_DATA_DIR=/var/lib/etcd/${NAME}.etcd
+#ipaddress=$(hostname -I | cut -f1 -d' ')
+#if test x$NAME == x; then
+#    NAME=$HOSTNAME
+#fi
+#export ETCD_NAME=${ETCD_NAME:-$HOSTNAME}
+#export ETCD_DATA_DIR=/var/lib/etcd/${NAME}.etcd
 
-sed -i s/ETCD_NAME.*/ETCD_NAME=$ETCD_NAME/g /etc/etcd/etcd.conf
-sed -i s/ETCD_DATA_DIR.*/ETCD_DATA_DIR=$ETCD_DATA_DIR/g /etc/etcd/etcd.conf
+
+#sed -i s/ETCD_NAME.*/ETCD_NAME=$ETCD_NAME/g /etc/etcd/etcd.conf
+#sed -i s/ETCD_DATA_DIR.*/ETCD_DATA_DIR=$ETCD_DATA_DIR/g /etc/etcd/etcd.conf
 
 source /etc/etcd/etcd.conf
 
